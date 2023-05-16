4fa9dd1dcb90cf503741726392bdc53f77a183f2
Seungju Han <sraq94@gmail.com>
2023-05-17 00:20:00 +0900

    postgre 연동

---
+++ src/main/java/com/honeydream/HoneydreamApplication.java
@@ -0,0 +1,13 @@
+package com.honeydream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoneydreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoneydreamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/honeydream/HoneydreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.honeydream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoneydreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
